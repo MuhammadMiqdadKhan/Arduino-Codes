7a0ade32bae8b9321b782e7b2a03e417dd6d7ef7
Muhammad Miqdad Khan <49819879+MMK08101997@users.noreply.github.com>
2019-12-25 20:28:05 +0500

    Add files via upload

---
+++ Line Following Robot/LFR.ino
@@ -0,0 +1,89 @@
+//LINE FOLLOWING ROBOT
+void motfwd();
+void motbwd();
+void motright();
+void motleft();
+void motstop();
+void setup() {
+  // put your setup code here, to run once:
+//motor 1
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);  //PWM   pin
+//motor 2
+pinMode(5,OUTPUT);  //PWM pin
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+//IRsensor  
+pinMode(2,INPUT);
+pinMode(3,INPUT);
+pinMode(4,INPUT);
+
+//Intializing serial
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Reading IR sensor
+int ir1 = digitalRead(2);
+int ir2 = digitalRead(3);
+int ir3 = digitalRead(4);
+Serial.print("middle");Serial.println(ir1);delay(1000);
+Serial.print("left");Serial.println(ir2);delay(1000);
+Serial.print("right");Serial.println(ir3);delay(1000);
+
+if((ir1 == LOW)&&(ir2 ==HIGH)&&(ir3 ==LOW)){
+  motfwd();
+}
+else if((ir1 == LOW)&&(ir2 ==LOW)&&(ir3 ==HIGH)){
+  motright();}
+
+else if((ir1 == HIGH)&&(ir2 ==LOW)&&(ir3 ==LOW)){
+  motleft();
+}
+}
+
+void motfwd(){
+  //MOTOR 1
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  analogWrite(10,218);
+  
+  //MOTOR 2
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  analogWrite(5,255); 
+}
+void motright(){
+  //MOTOR 1
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  analogWrite(10,218);
+  
+  //MOTOR 2
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+   
+}
+void motleft(){
+  //MOTOR 1
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  
+  //MOTOR 2
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  analogWrite(5,255);
+}
+
+void motstop(){
+  //MOTOR 1
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  
+  //MOTOR 2
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+}
+
---
+++ Obstacle Avoiding Robot/obstacleavoiding.ino
@@ -0,0 +1,47 @@
+// ---------------------------------------------------------------------------
+// Example NewPing library sketch that does a ping about 20 times per second.
+// ---------------------------------------------------------------------------
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN1  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN1     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE1 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
+#define TRIGGER_PIN2  5  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN2     6  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE2 200
+
+
+#define TRIGGER_PIN3  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN3     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE3 200
+
+
+NewPing sonar(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE1); // NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE2); 
+NewPing sonar(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE3); 
+
+void setup() {
+  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+}
+
+void loop() {
+  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS1 = sonar.ping();// Send ping, get ping time in microseconds (uS).
+   unsigned int uS2 = sonar.ping();
+    unsigned int uS3 = sonar.ping();
+  int distance1 = uS1 / US_ROUNDTRIP_CM;
+  int distance2 = uS2 / US_ROUNDTRIP_CM;
+  int distance3 = uS3 / US_ROUNDTRIP_CM;
+Serial.print("distance 1: ")Serial.print(distance1);Serial.print("\t");
+Serial.print("distance 2: ")Serial.print(distance2);Serial.print("\t");
+Serial.print("distance 3: ")Serial.print(distance3);Serial.print("\n");
+
+if(distance2<19){motright();}
+else if(distance2>19){motfwd();}
+else if(distance1<19){motleft();}
+else if(distance3<19){motleft();}
+else if(distance2<5){motstop();}
+}
